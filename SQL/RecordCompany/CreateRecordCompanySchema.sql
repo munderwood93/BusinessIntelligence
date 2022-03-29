6af59b4010d29b948be932bb28b4fc1270e78dae
munderwood93 <matthewaunderwood93@gmail.com>
2022-03-29 13:34:03 -0700

    Adding schema file, data file, and initial queires for RecordCompany practice database

---
+++ SQL/RecordCompany/CreateRecordCompanySchema.sql
@@ -0,0 +1,18 @@
+--Source: https://github.com/WebDevSimplified/Learn-SQL/blob/master/schema.sql
+CREATE DATABASE record_company;
+USE record_company;
+
+CREATE TABLE bands (
+  id INT,--NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE albums (
+  id INT,-- NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  release_year INT,
+  band_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (band_id) REFERENCES bands(id)
+);
---
+++ SQL/RecordCompany/IntroRecordCompanyQueries.sql
@@ -0,0 +1,118 @@
+/* 
+
+select query structure
+
+select [distint]
+	T1.table_col_name1 AS WhateverVarYouWantFromTable1,
+	T2.table_col_name1 AS WhateverVarYouWantFromTable2,
+	T1.table_col_name2,
+	T1.[table_col_name3] AS [Whatever Var You Want From Table 1]
+from databsename.dbo.table_name1 T1
+inner join table_name2 T2 ON T1.primkey = T2.forkey
+where T1.val = 'Matt' or T2.Val = 'Alyssa'
+ORDER BY T1.val DESC
+
+*/
+
+select * from record_company.dbo.bands;
+select * from record_company.dbo.albums;
+
+-- shows all the bands that have an album
+select 
+	bands.id AS BandID,
+	bands.name AS BandName,
+	albums.id AS AlbumID, 
+	albums.name AS AlbumName,
+	albums.release_year AS AlbumReleaseYear
+from record_company.dbo.bands bands
+inner join record_company.dbo.albums albums ON bands.id  = albums.band_id;
+
+
+-- shows all the bands regardless of if they have an album or not
+select 
+	bands.id AS BandID,
+	bands.name AS BandName,
+	albums.id AS AlbumID, 
+	albums.name AS AlbumName,
+	albums.release_year AS AlbumReleaseYear
+from record_company.dbo.bands bands
+left join record_company.dbo.albums albums ON bands.id  = albums.band_id;
+
+
+-- shows all the albumns regardless of if they have a corresponding band or not
+-- this happens to be the same result as inner join bc all albums have corresponding bands in these two tables
+select 
+	bands.id AS BandID,
+	bands.name AS BandName,
+	albums.id AS AlbumID, 
+	albums.name AS AlbumName,
+	albums.release_year AS AlbumReleaseYear
+from record_company.dbo.bands bands
+right join record_company.dbo.albums albums ON bands.id  = albums.band_id;
+
+-- shows all the bands that have an album where the band is Metallica 
+select 
+	bands.id AS BandID,
+	bands.name AS BandName,
+	albums.id AS AlbumID, 
+	albums.name AS AlbumName,
+	albums.release_year AS AlbumReleaseYear
+from record_company.dbo.bands bands
+inner join record_company.dbo.albums albums ON bands.id  = albums.band_id
+where bands.name = 'Metallica';
+
+select 
+	bands.id AS BandID,
+	bands.name AS BandName,
+	albums.id AS AlbumID, 
+	albums.name AS AlbumName,
+	albums.release_year AS AlbumReleaseYear
+from record_company.dbo.bands bands
+inner join record_company.dbo.albums albums ON bands.id  = albums.band_id
+where bands.id = 2;
+
+-- shows all the bands that have an album with no release year listed
+select 
+	bands.id AS BandID,
+	bands.name AS BandName,
+	albums.id AS AlbumID, 
+	albums.name AS AlbumName,
+	albums.release_year AS AlbumReleaseYear
+from record_company.dbo.bands bands
+inner join record_company.dbo.albums albums ON bands.id  = albums.band_id
+where albums.release_year is NULL;
+
+-- shows all the bands that have an album with a valid release year listed
+select 
+	bands.id AS BandID,
+	bands.name AS BandName,
+	albums.id AS AlbumID, 
+	albums.name AS AlbumName,
+	albums.release_year AS AlbumReleaseYear
+from record_company.dbo.bands bands
+inner join record_company.dbo.albums albums ON bands.id  = albums.band_id
+where albums.release_year is not NULL;
+
+-- shows all the bands that have an album with a valid release year listed in release year descending order
+select 
+	bands.id AS BandID,
+	bands.name AS BandName,
+	albums.id AS AlbumID, 
+	albums.name AS AlbumName,
+	albums.release_year AS AlbumReleaseYear
+from record_company.dbo.bands bands
+inner join record_company.dbo.albums albums ON bands.id  = albums.band_id
+where albums.release_year is not NULL
+order by albums.release_year desc;
+
+
+-- shows all the bands that have an album with a release year = 2018
+select 
+	bands.id AS BandID,
+	bands.name AS BandName,
+	albums.id AS AlbumID, 
+	albums.name AS AlbumName,
+	albums.release_year AS AlbumReleaseYear
+from record_company.dbo.bands bands
+inner join record_company.dbo.albums albums ON bands.id  = albums.band_id
+where albums.release_year = 2018
